70bef20600cc29ab4ebfd97dff3c90e66755d140
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-08-01 01:48:49 -0400

    Made frontend build independent

--- src/frontend/app/components/page.tsx
+++ src/frontend/app/components/page.tsx
@@ -2,14 +2,21 @@ import Component from './component'
 import ComponentCard from './component-card'
 
 async function getComponents() {
-    const res = await fetch(`${process.env.API_HOST}/components`, { cache: 'no-cache' })
-    const data = (await res.json()) as Component[]
-    return data
+    try {
+        const res = await fetch(`${process.env.API_HOST}/components`, { cache: 'no-cache' })
+        const data = (await res.json()) as Component[]
+        data.forEach((component, index) => {
+            const remove = () => data.splice(index, 1)
+            if (component.name === undefined) remove()
+        })
+        return data
+    } catch {}
+    return null
 }
 
 export default async function Components() {
     const components = await getComponents()
     return <div className='grid grid-cols-8 gap-2'>
-        {components.map(component => <ComponentCard key={component.name} component={component} />)}
+        {components?.map(component => <ComponentCard key={component.name} component={component} />)}
     </div>
 }
